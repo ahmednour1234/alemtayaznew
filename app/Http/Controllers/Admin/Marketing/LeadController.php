f3fda639e4480737e744577880ae26c44cd7bcc7
ahmednour1234 <ahmednour5999@gmail.com>
2026-06-06 14:17:37 +0300

    das

--- app/Http/Controllers/Admin/Marketing/LeadController.php
+++ app/Http/Controllers/Admin/Marketing/LeadController.php
@@ -23,9 +23,7 @@ class LeadController extends Controller
         $query = Lead::with(['campaign', 'branch', 'nationality', 'assignedAdmin', 'latestCallLog'])
             ->latest();
 
-        if ($me->isBranchAdmin()) {
-            $query->where('branch_id', $me->branch_id);
-        }
+
 
         // Filter: assigned to current user only
         if ($request->boolean('assigned_to_me')) {
